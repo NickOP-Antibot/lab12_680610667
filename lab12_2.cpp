ae6c867eaa265fc13206a73f0fac70f4c6362717
NickOP-Antibot <chitsanupat2549@gmail.com>
2025-12-30 15:11:33 +0700

    lab12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int, int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a, int b){
+    if(b==0) return a;
+    return gcd(b, a%b);
 }
