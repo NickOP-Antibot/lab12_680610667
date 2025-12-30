6fae2aed54d7d7bf9af954486b5b4decf1634835
NickOP-Antibot <chitsanupat2549@gmail.com>
2025-12-30 15:11:19 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(int a){
+    if (a <= 1){
+        if(a==0) return 0;
+        else return 1;
+    }
+    else {
+        return fibonacci(a-1) + fibonacci(a-2);
+    }
 }
